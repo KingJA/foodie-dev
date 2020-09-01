6de13559fed2862e55023c4d944e23d9aa407170
KingJA <kingjavip@gmail.com>
2020-09-01 16:50:05 +0800

    add sql

--- foodie-dev-service/src/main/java/com/imooc/service/StuService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/StuService.java
@@ -1,5 +1,7 @@
 package com.imooc.service;
 
+import com.imooc.pojo.Stu;
+
 /**
  * Description:TODO
  * Create Time:2020/9/1 0001 下午 4:10
@@ -7,4 +9,8 @@ package com.imooc.service;
  * Email:kingjavip@gmail.com
  */
 public interface StuService {
+    public Stu getStuInfo(int id);
+    public void saveStu();
+    public void updateStu(int id);
+    public void deleteStu(int id);
 }
